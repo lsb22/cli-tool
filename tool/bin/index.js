0fdeb0923a7e84c6dd70b9672f0b8bb267903811
lsb <likith15600@gmail.com>
2025-07-30 20:00:04 +0530

    add arg package to receive and process arguments

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 // this above line is being written to direct the use of node interpreter to execute this file
-console.log("hello tool");
+
+const arg = require("arg");
+
+try {
+  const args = arg({
+    // define the command line arguments that you except and their type
+    "--start": Boolean,
+    "--fun": String,
+  });
+  if (args["--start"]) console.log("Starting app");
+} catch (error) {
+  console.log(error.message);
+  console.log();
+  usage();
+}
+
+function usage() {
+  // function that tells the user how to use the command line tool
+  console.log(`tool [CMD]
+    --start\tStarts the app
+    --fun\tSome string input`);
+}
